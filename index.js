a2a2658cd91dde5de99356bbe416222cfdfd6a54
fuchunhui <fuchuhu7@gmail.com>
2021-04-27 18:26:50 +0800

    release: v0.1.3

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// @ts-check
+
+const { yellow, green, cyan, blue, red } = require('kolorist');
+
+async function init() {
+  console.log(yellow, green, cyan, blue, red);
+}
+
+init().catch((e) => {
+  console.error(e);
+});
