5891a9c46157aebb9a6f986e9deda9b7cea2497b
fuchunhui <fuchuhu7@gmail.com>
2021-04-29 17:46:10 +0800

    chore: remove typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,7 @@ const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
   root: true,
-  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2021
@@ -34,22 +33,6 @@ module.exports = defineConfig({
       }
     ],
     'node/no-unpublished-import': 'off',
-    'node/no-unpublished-require': 'off',
-
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
-  },
-  overrides: [
-    {
-      files: ['packages/create-app/template-*/**'],
-      rules: {
-        'node/no-missing-import': 'off'
-      }
-    }
-  ]
+    'node/no-unpublished-require': 'off'
+  }
 });
