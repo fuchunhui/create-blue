be9dddeae85c70e8bd02826de750e41374c51eea
fuchunhui <fuchuhu7@gmail.com>
2021-12-15 17:27:55 +0800

    feat: update commit

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -1,15 +1,18 @@
-/*eslint-disable*/
-const chalk = require('chalk');
-const path = require('path');
-const msgPath = path.resolve(__dirname, '../.git/COMMIT_EDITMSG');
-const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
+import chalk from 'chalk';
+import path from 'path';
+import * as fs from 'fs';
+
+const __dirname = path.resolve();
+
+const msgPath = path.resolve(__dirname, '.git/COMMIT_EDITMSG');
+const msg = fs.readFileSync(msgPath, 'utf-8').trim();
 const rule = /^(revert: )?(feat|fix|docs|style|refactor|test|build|ci|chore|types|wip|release|perf)(\(.+\))?: .{1,50}/;
 
 if (!rule.test(msg)) {
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
-      chalk.red(`  Proper commit message format is required for automated changelog generation. \n  Examples:\n\n`) +
-      `${chalk.green(`    feat(compiler): add 'comments' option`)}\n`
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n`
+      + chalk.red(`  Proper commit message format is required for automated changelog generation. \n  Examples:\n\n`)
+      + `${chalk.green(`    feat(compiler): add 'comments' option`)}\n`
   );
   process.exit(1);
 }
